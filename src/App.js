de7bf09d328cba5007392b3541e473214cdfe5fd
vishnuavva <avvr2001@gmail.com>
2025-02-17 11:52:47 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import './App.css';
 // import HomeDefault from './HomeDefault';
 // import SplashScreen from './splashscreen';
 //this app.js file
-import SignUp from './Logincandidatesigup';
+// import SignUp from './Logincandidatesigup';
+import OTPVerification from './SignUpverification';
 function App() {
   // const[loading,setLoading]=useState(true);
   return (
     <>
     {/* {loading ? <SplashScreen onFinish={()=>setLoading(false)}/> : <HomeDefault/>} */}
-    <SignUp />
+    {/* <SignUp /> */}
+    <OTPVerification />
     </>
   );
 }
