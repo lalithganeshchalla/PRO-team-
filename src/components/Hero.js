5f58dcd5645662da40b5834b16d6866de85be16e
venkatesh-dotcom <venkateshsirasanagandla@gmail.com>
2025-02-21 16:43:30 +0530

    profileicon and languages

---
+++ src/components/Hero.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Container, Typography, Box } from "@mui/material";
+import image1 from "./ccc.jpg";
+
+const Hero = () => {
+  return (
+    <Container sx={{ display: "flex", alignItems: "center", marginTop: 5 }}>
+      <img src={image1} alt="home" style={{ width: "200px" }} />
+      <Box sx={{ textAlign: "left", marginLeft: 5 }}>
+        <Typography variant="h4">
+          <span style={{ color: "#00cc00", fontWeight: "bold" }}>Master</span> of Your Career
+        </Typography>
+        <Typography variant="subtitle1" sx={{ color: "#666", margin: "10px 0" }}>
+          Discover worldwide opportunities to enhance your skills, build an impressive CV, and land your dream job
+        </Typography>
+      </Box>
+    </Container>
+  );
+};
+
+export default Hero;
---
+++ src/components/Home.js
@@ -0,0 +1,67 @@
+import React from "react";
+import { Container, Grid, Typography } from "@mui/material";
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import FeatureCard from "./FeatureCard";
+import CategoryCard from "./CategoryCard";
+
+import intenship from "./intenship.png";
+import mentors from "./mentors.png";
+import jobs from "./jobs.png";
+import more from "./more.png";
+import awades from "./awades.png";
+import aechives from "./aechives.png";
+import student from "./student.png";
+import compani from "./compani.png";
+import colleges from "./colleges.png";
+
+const Home = () => {
+  const features = [
+    { imgSrc: intenship, title: "Internships", desc: "Gain Practical Experience", color: "#cceeff" },
+    { imgSrc: jobs, title: "Jobs", desc: "Explore Diverse Careers", color: "#ffccdd" },
+    { imgSrc: aechives, title: "Competitions", desc: "Battle For Excellence", color: "#ffddaa" },
+    { imgSrc: mentors, title: "Mentors", desc: "Guidance From Top Mentors", color: "#ffffcc" },
+    { imgSrc: awades, title: "Practice", desc: "Refine Skills Daily", color: "#ccffcc" },
+    { imgSrc: more, title: "More", desc: "", color: "#e0c3fc" },
+  ];
+
+  const categories = [
+    { imgSrc:student, title: "Students and Professionals", desc: "Compete, Build Resume, Grow and get Hired!" },
+    { imgSrc:compani, title: "Companies and Recruiters", desc: "Hire, Engage, and Brand Like Never Before!" },
+    { imgSrc:colleges, title: "Colleges", desc: "Bridge Academia and Industry with Real-World Opportunities!" },
+  ];
+
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      
+      {/* Feature Cards */}
+      <Container sx={{ marginTop: 5 }}>
+        <Grid container spacing={3} sx={{ justifyContent: "flex-end" }}>
+          {features.map((item, index) => (
+            <Grid item xs={6} sm={4} key={index}>
+              <FeatureCard {...item} />
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+
+      {/* User Categories */}
+      <Container sx={{ marginTop: 5 }}>
+        <Typography variant="h6" sx={{ fontWeight: "bold", marginBottom: 2 }}>
+          Who's using Master Code?
+        </Typography>
+        <Grid container spacing={3} sx={{ justifyContent: "flex-end"}}>
+          {categories.map((item, index) => (
+            <Grid item xs={12} sm={4} key={index}>
+              <CategoryCard {...item} />
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    </>
+  );
+};
+
+export default Home;
